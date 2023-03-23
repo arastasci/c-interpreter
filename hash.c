416cec80052ae195339c7c1447dc6e41f1c6d16b
yasinbastug <oyba2003@gmail.com>
2023-03-23 15:27:31 +0300

    hash.c added to store variables

---
+++ hash.c
@@ -0,0 +1,32 @@
+int values[128];
+char* names[128];
+unsigned int hash(char *str)
+{
+    unsigned int hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
+int setValue(char *name, int value)
+{
+    int index = hash(*name) % 128;
+    while(names[index] != *name){
+        index++;
+    }
+    values[index] = value;
+    names[index] = *name;
+    return value;
+}
+int getValue(char *name)
+{
+    int index = hash(*name) % 128;
+    while(names[index] != *name){
+        index++;
+    }
+    return values[index];
+}
+
